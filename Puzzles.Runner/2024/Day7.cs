dd5faefdf2cad559c6352cb2eda4b0925580a2c6
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-07 20:22:47 +0200

    day-7/refactoring

--- Puzzles.Runner/2024/Day7.cs
+++ Puzzles.Runner/2024/Day7.cs
@@ -1,8 +1,12 @@
 ﻿namespace Puzzles.Runner._2024;
 
+using OpFunc = Func<ulong, ulong, ulong>;
+
 [Puzzle("Bridge Repair", 7, 2024)]
 public class Day7(ILinesInputReader input) : IPuzzleSolver
 {
+    private const int NUMBER_OF_TASKS = 128;
+
     ulong[] _answers = [];
     ulong[][] _input = [];
 
@@ -23,36 +27,45 @@ public class Day7(ILinesInputReader input) : IPuzzleSolver
     }
 
     public string SolvePart1()
-    {
-        ulong sum = 0;
-        foreach(var (input, index) in _input.WithIndex())
-        {
-            var answer = _answers[index];
-            if (Find2(input, 1, input[0], answer, Add) || 
-                Find2(input, 1, input[0], answer, Mult))
-                sum += answer;
-        }
-
-        return sum.ToString();
-    }
+        => CalculateSum([Add, Mult]).ToString();
 
     public string SolvePart2()
+        => CalculateSum([Add, Mult, Concat]).ToString();
+
+    #region Private methods
+
+    private ulong CalculateSum(OpFunc[] operations)
+    {
+        var chunkSize = (int)Math.Ceiling((double)_input.Length / NUMBER_OF_TASKS);
+        var tasks = Enumerable.Range(0, NUMBER_OF_TASKS)
+            .Select(i => CalculateSumAsync(operations, i * chunkSize, chunkSize))
+            .ToArray();
+
+        Task.WaitAll(tasks);
+        var sum = tasks.Aggregate(0UL, (acc, t) => acc += t.Result);
+
+        return sum;
+    }
+
+    private Task<ulong> CalculateSumAsync(OpFunc[] operations, int start, int count)
+        => Task.Run(() => CalculateSum(operations, start, count));
+
+    private ulong CalculateSum(OpFunc[] operations, int start, int count)
     {
         ulong sum = 0;
-        foreach (var (input, index) in _input.WithIndex())
-        {
-            var answer = _answers[index];
-            if (Find3(input, 1, input[0], answer, Add) ||
-                Find3(input, 1, input[0], answer, Mult) ||
-                Find3(input, 1, input[0], answer, Concat))
+        var end = Math.Min(start + count, _input.Length);
 
+        for (int i = start; i < end; i++)
+        {
+            var answer = _answers[i];
+            if (Find(_input[i], operations, 0, 0, answer, Add))
                 sum += answer;
         }
 
-        return sum.ToString();
+        return sum;
     }
 
-    public static bool Find2(ulong[] arr, int index, ulong acc, ulong answer, Func<ulong, ulong, ulong> operation)
+    private static bool Find(ulong[] arr, OpFunc[] operations, int index, ulong acc, ulong answer, OpFunc operation)
     {
         if (acc > answer)
             return false;
@@ -62,32 +75,24 @@ public class Day7(ILinesInputReader input) : IPuzzleSolver
 
         var value = operation(acc, arr[index]);
 
-        return Find2(arr, index + 1, value, answer, Add) ||
-            Find2(arr, index + 1, value, answer, Mult);
+        var result = false;
+        for (int i = 0; !result && i < operations.Length; i++)
+            result |= Find(arr, operations, index + 1, value, answer, operations[i]);
+
+        return result;
     }
 
-    public static bool Find3(ulong[] arr, int index, ulong acc, ulong answer, Func<ulong, ulong, ulong> operation)
-    {
-        if (acc > answer)
-            return false;
+    #region Operations
 
-        if(index >= arr.Length)
-            return acc == answer;
-
-        var value = operation(acc, arr[index]);
-
-        return Find3(arr, index + 1, value, answer, Add) ||
-            Find3(arr, index + 1, value, answer, Mult) ||
-            Find3(arr, index + 1, value, answer, Concat);
-    }
-
-    public static ulong Mult(ulong a, ulong b)
+    private static ulong Mult(ulong a, ulong b)
         => a * b;
 
-    public static ulong Add(ulong a, ulong b)
+    private static ulong Add(ulong a, ulong b)
         => a + b;
 
-    public static ulong Concat(ulong a, ulong b)
+    private static ulong Concat(ulong a, ulong b)
         => a * (ulong)Math.Pow(10, (ulong)Math.Log10(b) + 1UL) + b;
-    
+
+    #endregion
+    #endregion
 }
