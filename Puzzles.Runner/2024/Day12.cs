eaa5639a02618a74abe1aba015531bd52dac705f
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-12 14:50:07 +0200

    day-12/cleanup

--- Puzzles.Runner/2024/Day12.cs
+++ Puzzles.Runner/2024/Day12.cs
@@ -14,42 +14,68 @@ public class Day12(ILinesInputReader input) : IPuzzleSolver
     private char[] _map = [];
     private int[] _ids = [];
 
+    private int _idCount;
+
     private int _sizeX = 0;
+    private Queue<int> _queue = [];
 
     public void Init()
     {
         InitMap();
         CalculateIds();
     }
-
+   
     public string SolvePart1()
     {
-        Dictionary<int, int> p = [];
-        Dictionary<int, int> a = [];
-        Dictionary<char, HashSet<int>> groups = [];
+        var p = new int[_idCount];
+        var a = new int[_idCount];
 
         for (int i = _sizeX; i < _map.Length - _sizeX; i++)
         {
             if (_map[i] == BORDER)
                 continue;
 
-            var id = _ids[i];
-            var val = _map[i];
-
-            a.TryAdd(id, 0);
+            var (id, val) = (_ids[i], _map[i]);
             a[id]++;
 
-            p.TryAdd(id, 0);
             for (int d = 0; d < _directions.Length; d++)
                 if (id != _ids[i + _directions[d]])
                     p[id]++;
-
-            groups.TryAdd(val, []);
-            groups[val].Add(id);
         }
 
-        return groups.Sum(kv => kv.Value.Sum(i => p[i] * a[i])).ToString();
+        return p.Zip(a, (pv, av) => pv * av).Sum().ToString();
     }
+
+    public string SolvePart2()
+    {
+        var p = new int[_idCount];
+        var a = new int[_idCount];
+
+        for (int i = _sizeX; i < _map.Length - _sizeX; i++)
+        {
+            if (_map[i] == BORDER)
+                continue;
+
+            var (id, val) = (_ids[i], _map[i]);            
+            a[id]++;            
+            
+            for (int d = 0; d < _directions.Length; d++)
+            {
+                var d1 = _directions[d];
+                var d2 = _directions[(d + 1) % _directions.Length];
+
+                if (id != _ids[i + d1] && id != _ids[i + d2])
+                    p[id]++;
+
+                if (id == _ids[i + d1] && id == _ids[i + d2] && id != _ids[i + d1 + d2])
+                    p[id]++;
+            }
+        }
+
+        return p.Zip(a, (pv, av) => pv * av).Sum().ToString();
+    }
+
+    #region Private methods
 
     private void InitMap()
     {
@@ -69,21 +95,22 @@ public class Day12(ILinesInputReader input) : IPuzzleSolver
                 _map[Mat2Vec(x + 1, y + 1)] = input.Lines[y][x];
 
         _directions = [-sy, 1, sy, -1];
+        _queue = new Queue<int>(_map.Length);
     }
 
-    private Queue<int> _queue = [];
+
     private void CalculateIds()
     {
-        _queue = new Queue<int>(_map.Length);
         _ids = new int[_map.Length];
         Array.Fill(_ids, NO_ID);
 
-        for(int i = _sizeX, id = 0; i < _ids.Length - _sizeX; i++, id++)
+        _idCount = 0;
+        for (int i = _sizeX; i < _ids.Length - _sizeX; i++, _idCount++)
         {
             if (_map[i] == BORDER || _ids[i] != NO_ID)
                 continue;
 
-            _ids[i] = id;
+            _ids[i] = _idCount;
 
             _queue.Clear();
             _queue.Enqueue(i);
@@ -96,7 +123,7 @@ public class Day12(ILinesInputReader input) : IPuzzleSolver
                 {
                     if (_map[current + d] != BORDER && _ids[current + d] == NO_ID && _map[current] == _map[current + d])
                     {
-                        _ids[current + d] = id;
+                        _ids[current + d] = _idCount;
                         _queue.Enqueue(current + d);
                     }
                 });
@@ -106,4 +133,6 @@ public class Day12(ILinesInputReader input) : IPuzzleSolver
 
     private int Mat2Vec(int x, int y)
         => y * _sizeX + x;
+
+    #endregion
 }
