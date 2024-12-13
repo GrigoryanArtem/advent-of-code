b420b1c5fa016da750f91f1e74b0676d65b3a4c7
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-13 16:23:17 +0200

    day-13/refactoring

--- Puzzles.Runner/2024/Day13.cs
+++ Puzzles.Runner/2024/Day13.cs
@@ -5,63 +5,50 @@ namespace Puzzles.Runner._2024;
 [Puzzle("Claw Contraption", 13, 2024)]
 public partial class Day13(IFullInputReader input) : IPuzzleSolver
 {
-    private record ClawMachine(decimal[] Mat, decimal[] Sol);
-    private const decimal P2_K = 10000000000000;
+    private record ClawMachine(long[] Mat, long[] Sol);
+
+    #region Constants
+
+    private readonly string[] MatTemplate = ["ax", "bx", "ay", "by"];
+    private readonly string[] SolTemplate = ["px", "py"];
+
+    private const long P2_K = 10000000000000;
+
+    #endregion
+
     private ClawMachine[] _machines = [];
 
     public void Init() => _machines = PuzzleRegex()
         .Matches(input.Text)
         .Select(m => new ClawMachine
         (
-            Mat:
-            [
-                G2D(m, "ax"), G2D(m, "bx"),
-                G2D(m, "ay"), G2D(m, "by"),
-            ],
-            Sol:
-            [
-                G2D(m, "px"),
-                G2D(m, "py")
-            ]
+            Mat: [.. MatTemplate.Select(t => M2L(m, t))],
+            Sol: [.. SolTemplate.Select(t => M2L(m, t))]
         )).ToArray();
 
     public string SolvePart1()
-        => _machines.Where(p => Det(p.Mat) != 0)
-            .Select(p => MatByVec(Inv(p.Mat), p.Sol))
-            .Sum(r => IsValid(r) ? Math.Round(r[0] * 3 + r[1]) : 0m)
-            .ToString("f0");
+        => _machines.Sum(m => CalculatePrice(m)).ToString();
 
     public string SolvePart2()
-        => _machines.Where(p => Det(p.Mat) != 0)
-            .Select(p => MatByVec(Inv(p.Mat), [..p.Sol.Select(s => s + P2_K)]))
-            .Sum(r => IsValid(r) ? Math.Round(r[0] * 3 + r[1]) : 0m)
-            .ToString("f0");
+        => _machines.Sum(m => CalculatePrice(m, P2_K)).ToString();
 
     #region Private methods
 
-    // Determinant
-    private static decimal Det(decimal[] mat)
-        => (mat[0] * mat[3]) - (mat[1] * mat[2]);
-
-    // Inverse
-    private static decimal[] Inv(decimal[] mat)
+    private static long CalculatePrice(ClawMachine machine, long solDiff = 0L)
     {
-        var invDet = 1 / Det(mat);
-        return [invDet * mat[3], -invDet * mat[1], -invDet * mat[2], invDet * mat[0]];
+        var (mat, sol) = (machine.Mat, machine.Sol);
+        var det = (mat[0] * mat[3]) - (mat[1] * mat[2]);
+        if (det == 0L)
+            return 0L;
+
+        var a = mat[3] * (sol[0] + solDiff) + -mat[1] * (sol[1] + solDiff);
+        var b = -mat[2] * (sol[0] + solDiff) + mat[0] * (sol[1] + solDiff);
+
+        return a % det == 0L && b % det == 0L ? a / det * 3L + (b / det) : 0L;
     }
 
-    // Multiply matrix by vector
-    private static decimal[] MatByVec(decimal[] mat, decimal[] vec) => 
-    [
-        mat[0] * vec[0] + mat[1] * vec[1],
-        mat[2] * vec[0] + mat[3] * vec[1] 
-    ];
-
-    private static decimal G2D(Match match, string group)
-        => Convert.ToDecimal(match.Groups[group].Value);
-
-    private static bool IsValid(decimal[] vec)
-        => vec.All(v => Math.Abs(v - Math.Round(v)) < 1e-7m);
+    private static long M2L(Match match, string group)
+        => Convert.ToInt64(match.Groups[group].Value);
 
     [GeneratedRegex(@"Button A: X\+(?<ax>\d+), Y\+(?<ay>\d+)\s+Button B: X\+(?<bx>\d+), Y\+(?<by>\d+)\s+Prize: X\=(?<px>\d+), Y\=(?<py>\d+)", RegexOptions.Compiled)]
     private static partial Regex PuzzleRegex();
