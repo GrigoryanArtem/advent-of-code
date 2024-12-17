16200e357928b9edacd15326f17057794c2af727
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-17 16:41:09 +0200

    day-17/p2

--- Puzzles.Runner/2024/Day17.cs
+++ Puzzles.Runner/2024/Day17.cs
@@ -1,33 +1,37 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Puzzles.Runner._2024;
 
 [Puzzle("Chronospatial Computer", 17, 2024)]
 public partial class Day17(ILinesInputReader input) : IPuzzleSolver
 {
+    private delegate void Instruction(long a, long b, ref long output);
+
+    private enum Code
+    {
+        ADV = 0,
+        BXL = 1,
+        BST = 2,
+        JNZ = 3,
+        BXC = 4,
+        OUT = 5,
+        BDV = 6,
+        CDV = 7,
+    }
+
+    #region Constants
+
     private const int PROGRAM_INDEX = 4;
     private const int NUMBER_OF_REGISTERS = 3;
 
     private const int A = 0;
     private const int B = 1;
-    private const int C = 2;
+    private const int C = 2;    
 
-    private const int ADV = 0;
-    private const int BXL = 1;
-    private const int BST = 2;
-    private const int JNZ = 3;
-    private const int BXC = 4;
-    private const int OUT = 5;
-    private const int BDV = 6;
-    private const int CDV = 7;
+    #endregion
 
-
-    public long[] _init = [];
-
-    public long[] _r = new long[NUMBER_OF_REGISTERS];
-    public byte[] _p = [];
-    public List<byte> _out = [];
+    public long[] _init = [];    
+    public byte[] _p = [];    
 
     public void Init()
     {
@@ -43,71 +47,87 @@ public partial class Day17(ILinesInputReader input) : IPuzzleSolver
 
     public string SolvePart1()
     {
-        ResetState();
-
-        for (int op = 0; op < _p.Length;)
-        {
-            var opcode = _p[op];
-            op = RunOp(op, _p[op + 1]);
-        }
-
-        return String.Join(",", _out);
+        long[] registers = new long[NUMBER_OF_REGISTERS];
+        Array.Copy(_init, registers, _init.Length);
+        return String.Join(",", Run(registers));
     }
 
-    private int RunOp(int op, int literal) => _p[op] switch
+    public string SolvePart2()
     {
-        ADV => op + Dv(_r[A], Combo(literal), ref _r[A]),
-        BDV => op + Dv(_r[A], Combo(literal), ref _r[B]),
-        CDV => op + Dv(_r[A], Combo(literal), ref _r[C]),
+        var a = 0L;
+        while (true)
+        {            
+            var output = Run([a, 0, 0]);
+            var match = _p.TakeLast(output.Length)
+                .Zip(output, (p, ot) => p == ot)                
+                .All(b => b);
 
-        BST => op + Mod(Combo(literal), ref _r[B]),
+            if (match && output.Length == _p.Length)
+                break;
 
-        BXL => op + Xor(_r[B], literal, ref _r[B]),
-        BXC => op + Xor(_r[B], _r[C], ref _r[B]),
+            a = match ? a << 3 : a + 1;
+        }
 
-        JNZ => Jmp(op, literal),
-        OUT => op + Out(Combo(literal)),
+        return a.ToString();
+    }
+
+    private byte[] Run(long[] registers)
+    {
+        List<byte> output = [];     
+
+        for (int opIdx = 0; opIdx < _p.Length;)        
+            opIdx = RunOp(registers, output, opIdx, _p[opIdx + 1], Combo(registers, _p[opIdx + 1]));
+
+        return [.. output];
+    }
+
+    private int RunOp(long[] reg, List<byte> output, int opIdx, int literal, long combo) => (Code)_p[opIdx] switch
+    {
+        Code.ADV => Do(opIdx, reg[A], combo, ref reg[A], Dv),
+        Code.BDV => Do(opIdx, reg[A], combo, ref reg[B], Dv),
+        Code.CDV => Do(opIdx, reg[A], combo, ref reg[C], Dv),
+        
+        Code.BST => Do(opIdx, combo, 8, ref reg[B], Mod),
+        
+        Code.BXL => Do(opIdx, reg[B], literal, ref reg[B], Xor),
+        Code.BXC => Do(opIdx, reg[B], reg[C], ref reg[B], Xor),        
+        
+        Code.JNZ => Jmp(opIdx, reg[A], literal),
+        Code.OUT => opIdx + Out(output, combo),
 
         _ => throw new NotImplementedException()
     };
 
-    private int Out(long literal)
-    {
-        _out.Add((byte)(literal % 8));
-        return 2;
-    }
-    private int Jmp(int op, int literal)
-        => _r[A] == 0 ? op + 2 : literal;
+    #region Operations
 
-    private int Mod(long a, ref long output)
+    private static int Do(int opIdx, long a, long b, ref long output, Instruction instruction)
     {
-        output = a % 8;
-        return 2;
+        instruction(a, b, ref output);
+        return opIdx + 2;
     }
 
-    private int Xor(long a, long b, ref long output)
+    private static int Out(List<byte> output, long literal)
     {
-        output = a ^ b;
+        output.Add((byte)(literal % 8));
         return 2;
     }
 
-    private int Dv(long a, long b, ref long output)
-    {
-        output = (long)(a / Math.Pow(2, b));
-        return 2;
-    }
+    private static int Jmp(int op, long check, int literal)
+        => check == 0 ? op + 2 : literal;
 
-    private long Combo(int n) => n switch
-    {
-        >=4 => _r[n - 4],
-        _ => n
-    };
+    private static void Mod(long a, long b, ref long output)
+        => output = a % b;
 
-    private void ResetState()
-    {
-        Array.Copy(_init, _r, _init.Length);
-        _out.Clear();
-    }
+    private static void Xor(long a, long b, ref long output)
+        => output = a ^ b;
+
+    private static void Dv(long a, long b, ref long output)
+        => output = a / (1L << (int)b);
+
+    private static long Combo(long[] r, int n)
+        => n >= 4 ? r[n - 4] : n;
+
+    #endregion
 
     [GeneratedRegex(@"\d+")]
     private static partial Regex NumRegex();
