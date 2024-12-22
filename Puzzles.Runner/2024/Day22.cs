58cea095f5edaf2517d9deb95d074fd2741925a4
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-22 19:36:10 +0200

    day-22/optimizations

--- Puzzles.Runner/2024/Day22.cs
+++ Puzzles.Runner/2024/Day22.cs
@@ -1,4 +1,6 @@
-﻿namespace Puzzles.Runner._2024;
+﻿using System.Collections.Concurrent;
+
+namespace Puzzles.Runner._2024;
 
 [Puzzle("Monkey Market", 22, 2024)]
 public class Day22(ILinesInputReader input) : IPuzzleSolver
@@ -8,6 +10,7 @@ public class Day22(ILinesInputReader input) : IPuzzleSolver
     private const long MOD = 16777216L;
     private const long SEQ_SIZE = 4;
     private const int ITERATIONS = 2000;
+    private const int BASE_10 = 10;
 
     #endregion
 
@@ -23,20 +26,9 @@ public class Day22(ILinesInputReader input) : IPuzzleSolver
 
     public string SolvePart2()
     {
-        var diffs = _numbers.AsParallel()
-            .Select(n => CalculateSeqs(n, ITERATIONS))
-            .ToArray();
-
-        var seqs = diffs
-            .SelectMany(d => d.Keys)
-            .Distinct()
-            .ToArray();
-
-        return seqs.AsParallel()
-            .Select(k => diffs.Sum(d => d.GetValueOrDefault(k, 0)))
-            .OrderByDescending(s => s)
-            .First()
-            .ToString();
+        var sums = new ConcurrentDictionary<int, long>();
+        _numbers.AsParallel().ForAll(n => CalculateSeqs(n, ITERATIONS, sums));            
+        return sums.Values.AsParallel().Max().ToString();
     }
 
     #region Private methods
@@ -49,54 +41,35 @@ public class Day22(ILinesInputReader input) : IPuzzleSolver
         return secret;
     }
 
-    private static Dictionary<int, long> CalculateSeqs(long secret, long iterations)
+    private static void CalculateSeqs(long secret, long iterations, ConcurrentDictionary<int, long> sums)
     {
-        Dictionary<int, long> result = [];
+        var added = new HashSet<int>();
+        iterations -= SEQ_SIZE;
 
-        var buffer = new int[SEQ_SIZE];
-        var tail = 0L;
+        var seq = 0;                
         var prev = 0L;
 
-        long updateBuffer()
+        void update()
         {
-            var digit = secret % 10;
-            buffer[tail] = (int)(digit - prev) + 10;
-
-            return digit;
-        }
-
-        void next(long digit)
-        {
+            var digit = secret % BASE_10;
+            seq = (seq << 8) + (byte)(digit - prev + BASE_10);
             prev = digit;
-            tail = (tail + 1L) % SEQ_SIZE;
-            secret = NextSecret(secret);
         }
 
-        for (int i = 0; i < SEQ_SIZE; i++)
-            next(updateBuffer());
+        for (int i = 0; i < SEQ_SIZE; secret = NextSecret(secret), i++)        
+            update();
+        
 
-        iterations -= SEQ_SIZE;
-        for (int i = 0; i < iterations; i++)
+        for (int i = 0; i < iterations; secret = NextSecret(secret), i++)
         {
-            var digit = updateBuffer();
+            update();
 
-            var seq = B2S(buffer, tail);
-            result.TryAdd(seq, digit);
-
-            next(digit);
+            if (!added.Contains(seq))
+            {
+                sums.AddOrUpdate(seq, prev, (_, v) => v + prev);
+                added.Add(seq);
+            }
         }
-
-        return result;
-    }
-
-    private static int B2S(int[] buffer, long tail)
-    {
-        var answer = 0;
-
-        for (int i = 1; i <= SEQ_SIZE; i++)
-            answer = (answer << 5) + buffer[(tail + i) % SEQ_SIZE];
-
-        return answer;
     }
 
     private static long NextSecret(long secret)
