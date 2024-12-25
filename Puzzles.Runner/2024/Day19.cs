45e7319fd00daed32c2b9d44c3a7b53622d534d6
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-25 22:29:17 +0200

    day-19/fix

--- Puzzles.Runner/2024/Day19.cs
+++ Puzzles.Runner/2024/Day19.cs
@@ -6,18 +6,16 @@ namespace Puzzles.Runner._2024;
 public class Day19(ILinesInputReader input) : IPuzzleSolver
 {
     private ConcurrentDictionary<string, ulong> _cache = [];
-
-    private string[] _towels = [];
+    private HashSet<string> _towels = [];
     private string[] _patterns = [];
 
     public void Init()
     {
         var tokens = input.GetTokens(",", s => s);
 
-        _towels = tokens[0];
+        _towels = [.. tokens[0]];
         _patterns = tokens.Skip(2).Select(t => t.First()).ToArray();
     }
-
     public string SolvePart1()
     {
         _cache = new(_towels.ToDictionary(t => t, _ => 1UL));
@@ -28,6 +26,6 @@ public class Day19(ILinesInputReader input) : IPuzzleSolver
         => _patterns.UInt64Sum(DesignCombinations).ToString();
 
     public ulong DesignCombinations(string pattern)
-        => _cache.GetOrAdd(pattern, _towels.Where(pattern.StartsWith)
-            .UInt64Sum(t => DesignCombinations(pattern[t.Length..])));
+        => _cache.TryGetValue(pattern, out var value) ? value : _cache.GetOrAdd(pattern, 
+            _towels.Where(pattern.StartsWith).UInt64Sum(t => DesignCombinations(pattern[t.Length..])));
 }
