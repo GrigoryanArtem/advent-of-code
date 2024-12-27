ed88d37bbc77133ae1944f7b05664442f0194f05
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-12-27 11:13:01 +0200

    2019/day-2

---
+++ Puzzles.Runner/2019/Common/IntCodeMachine.cs
@@ -0,0 +1,76 @@
+﻿namespace Puzzles.Runner._2019.Common;
+
+public class IntCodeMachine
+{
+    public int[] _init;    
+    public int[] _memory;    
+
+    public IntCodeMachine(int[] memory)
+    {
+        _init = memory;
+        _memory = new int[memory.Length];
+
+        Reset();
+    }
+
+    public int Size => _memory.Length;
+
+    public int this[int idx] => _memory[idx];
+
+    public int Noun
+    {
+        get => _memory[1];
+        set => _memory[1] = value;
+    }
+
+    public int Verb
+    {
+        get => _memory[2];
+        set => _memory[2] = value;
+    }
+
+    public int Result => _memory[0];
+
+    public void Reset()
+        => Array.Copy(_init, _memory, _memory.Length);
+
+    public void Reset(int noun, int verb)
+    {
+        Array.Copy(_init, _memory, _memory.Length);
+
+        Noun = noun;
+        Verb = verb;
+    }
+
+    public void Run()
+    {
+        for (int i = 0; i < _memory.Length; i += 4)
+        {
+            var op = _memory[i];
+
+            if (op == 99)
+                break;
+
+            RunOp(op, _memory[_memory[i + 1]], _memory[_memory[i + 2]], ref _memory[_memory[i + 3]]);
+        }
+    }
+
+    private static void RunOp(int op, int a, int b, ref int r)
+    {
+        switch (op)
+        {
+            case 1:
+                Add(a, b, ref r);
+                break;
+            case 2:
+                Multiply(a, b, ref r);
+                break;
+        }
+    }
+
+    private static void Add(int a, int b, ref int r)
+        => r = a + b;
+
+    private static void Multiply(int a, int b, ref int r)
+        => r = a * b;
+}
---
+++ Puzzles.Runner/2019/Day02.cs
@@ -0,0 +1,38 @@
+﻿using Puzzles.Runner._2019.Common;
+
+namespace Puzzles.Runner._2019;
+
+[Puzzle("1202 Program Alarm", 2, 2019)]
+public class Day02(ILinesInputReader input) : IPuzzleSolver
+{
+    private const int EXPECTED_RESULT = 19690720;
+    private IntCodeMachine _machine;
+
+    public void Init()
+        => _machine = new([.. input.GetTokens(",", Convert.ToInt32).First()]);
+
+    public string SolvePart1()
+    {        
+        _machine.Reset(noun: 12, verb: 2);
+        _machine.Run();
+
+        return _machine.Result.ToString();
+    }
+
+    public string SolvePart2()
+    {        
+        for (int noun = 0; noun <= 99; noun++)
+        {
+            for (int verb = 0; verb <= 99; verb++)
+            {
+                _machine.Reset(noun, verb);
+                _machine.Run();
+
+                if (_machine.Result == EXPECTED_RESULT)
+                    return (100 * noun + verb).ToString();
+            }
+        }
+
+        return String.Empty;
+    }    
+}
