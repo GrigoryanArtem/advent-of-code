eaf2670d46f757dc9ef592c2ced073b74d4d9f7d
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-01-09 18:16:00 +0200

    2019/day-5/p1

--- Puzzles.Runner/2019/Common/IntCodeMachine.cs
+++ Puzzles.Runner/2019/Common/IntCodeMachine.cs
@@ -2,8 +2,13 @@
 
 public class IntCodeMachine
 {
-    public int[] _init;    
-    public int[] _memory;    
+    private static readonly int[] MODE = [100, 1000, 10000];
+
+    public int[] _init;
+    public int[] _memory;
+
+    public List<int> _output = [];
+    public Queue<int> _input = [];
 
     public IntCodeMachine(int[] memory)
     {
@@ -13,7 +18,9 @@ public class IntCodeMachine
         Reset();
     }
 
+    public int State { get; private set; }
     public int Size => _memory.Length;
+    public IEnumerable<int> Output => _output;
 
     public int this[int idx] => _memory[idx];
 
@@ -31,12 +38,19 @@ public class IntCodeMachine
 
     public int Result => _memory[0];
 
-    public void Reset()
-        => Array.Copy(_init, _memory, _memory.Length);
-
-    public void Reset(int noun, int verb)
+    public void Reset(int[]? input = null)
     {
         Array.Copy(_init, _memory, _memory.Length);
+
+        _input = new(input ?? []);
+        _output.Clear();
+
+        State = 0;
+    }
+
+    public void Reset(int noun, int verb, int[]? input = null)
+    {
+        Reset(input);
 
         Noun = noun;
         Verb = verb;
@@ -44,33 +58,56 @@ public class IntCodeMachine
 
     public void Run()
     {
-        for (int i = 0; i < _memory.Length; i += 4)
+        while(true)
         {
-            var op = _memory[i];
+            var op = _memory[State];
+            var opCode = OpCode(op);
 
-            if (op == 99)
+            if (opCode == 99)
                 break;
 
-            RunOp(op, _memory[_memory[i + 1]], _memory[_memory[i + 2]], ref _memory[_memory[i + 3]]);
+            State += opCode switch
+            {
+                1 => Add(Val(op, 1), Val(op, 2), _memory[State + 3]),
+                2 => Multiply(Val(op, 1), Val(op, 2), _memory[State + 3]),
+                3 => In(_memory[State + 1]),
+                4 => Out(_memory[State + 1]),
+                _ => throw new InvalidOperationException($"Invalid opcode: {opCode}")
+            };
         }
     }
 
-    private static void RunOp(int op, int a, int b, ref int r)
+    private int Add(int a, int b, int c)
     {
-        switch (op)
-        {
-            case 1:
-                Add(a, b, ref r);
-                break;
-            case 2:
-                Multiply(a, b, ref r);
-                break;
-        }
+        _memory[c] = a + b;
+        return 4;
     }
 
-    private static void Add(int a, int b, ref int r)
-        => r = a + b;
+    private int Multiply(int a, int b, int c)
+    {
+        _memory[c] = a * b;
+        return 4;
+    }
 
-    private static void Multiply(int a, int b, ref int r)
-        => r = a * b;
+
+    private int In(int a)
+    {
+        _memory[a] = _input.Dequeue();
+        return 2;
+    }
+    
+    private int Out(int a)
+    {
+        _output.Add(_memory[a]);
+        return 2;
+    }
+
+    private int Val(int op, int parameter)
+        => V2M(op, parameter) ? _memory[State + parameter] : _memory[_memory[State + parameter]];
+
+    private static int OpCode(int address)
+        => address % MODE[0];
+
+    private static bool V2M(int value, int parameter)
+        => ((value / MODE[parameter - 1]) & 1) == 1;
 }
---
+++ Puzzles.Runner/2019/Day05.cs
@@ -0,0 +1,20 @@
+﻿using Puzzles.Runner._2019.Common;
+
+namespace Puzzles.Runner._2019;
+
+[Puzzle("Sunny with a Chance of Asteroids", 5, 2019)]
+public class Day05(ILinesInputReader input) : IPuzzleSolver
+{
+    private IntCodeMachine _machine;
+
+    public void Init()
+        => _machine = new([.. input.GetTokens(",", Convert.ToInt32).First()]);
+
+    public string SolvePart1()
+    {
+        _machine.Reset([1]);
+        _machine.Run();
+
+        return _machine.Output.Last().ToString();
+    }
+}
