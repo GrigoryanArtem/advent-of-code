5e6f087c139f726503b0ec0ef72aa16d19929e62
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-11-04 16:24:58 +0200

    2022/day-4

---
+++ Puzzles.Runner/2022/Day04.cs
@@ -0,0 +1,38 @@
+﻿namespace Puzzles.Runner._2022;
+
+[Puzzle("Camp Cleanup", 4, 2022)]
+public class Day04(ILinesInputReader input) : IPuzzleSolver
+{
+    private record Range(int Start, int End);
+    private (Range r1, Range r2)[] _ranges = [];
+
+    public void Init() 
+        => _ranges = [..input.GetTokens(",", T2R).Select(t => Sort(t.First(), t.Last()))];    
+
+    public string SolvePart1()
+        => _ranges.Count(t => Contains(t.r1, t.r2)).ToString();
+    
+    public string SolvePart2()
+        => _ranges.Count(t => Overlap(t.r1, t.r2)).ToString();
+
+    private static bool Contains(Range r1, Range r2)
+        => r1.Start <= r2.Start && r1.End >= r2.End;
+    
+    private static bool Overlap(Range r1, Range r2)
+        => r2.Start >= r1.Start && r2.Start <= r1.End ||
+            r2.End >= r1.Start && r2.End <= r1.End;
+
+    private static (Range r1, Range r2) Sort(Range r1, Range r2)
+    {
+        if ((r2.End - r2.Start) > (r1.End - r1.Start))
+            (r1, r2) = (r2, r1);
+
+        return (r1, r2);
+    }
+
+    private static Range T2R(string token)
+    {
+        var parts = token.Split("-", 2).Select(Int32.Parse).ToArray();
+        return new(parts[0], parts[1]);
+    }
+}
