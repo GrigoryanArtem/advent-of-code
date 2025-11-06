f490ab333327f3518e9eb4e1a044b58b085c251f
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-11-07 00:21:19 +0200

    2022/day-10

---
+++ Puzzles.Runner/2022/Day10.cs
@@ -0,0 +1,61 @@
+﻿namespace Puzzles.Runner._2022;
+
+[Puzzle("Rope Bridge", 10, 2022)]
+public class Day10(ILinesInputReader input) : IPuzzleSolver
+{
+    private record Instruction(string Opcode, int Value);
+
+    private Instruction[] _program = [];
+
+    public void Init()
+    {
+        _program = [..input.Lines.Select(line =>
+        {
+            var tokens = line.Split(' ', 2);
+            return new Instruction(tokens[0], tokens.Length > 1 ? Convert.ToInt32(tokens[1]) : 0);
+        })];
+    }
+
+    public string SolvePart1()
+    {
+        var e = Run(_program).GetEnumerator();
+
+        Loop(e, 20);
+        long sum = 20 * e.Current;
+        
+        for(int i = 1; i <= 5; i++)
+        {
+            Loop(e, 40);
+            sum += (20 + i * 40) * e.Current;
+        }
+
+        return sum.ToString();
+    }
+
+    private void Loop(IEnumerator<int> enumerator, int cycles)
+    {
+        for (int i = 0; i < cycles; i++)
+            enumerator.MoveNext();
+    }
+
+    private IEnumerable<int> Run(Instruction[] program) 
+    {
+        int x = 1;
+
+        foreach (var instr in program)
+        {
+            if(instr.Opcode == "noop")
+                yield return x;
+
+            if(instr.Opcode == "addx")
+            {
+                yield return x;
+                yield return x;
+                x += instr.Value;
+            }
+        }
+
+        while (true)
+            yield return x;
+    }
+}
