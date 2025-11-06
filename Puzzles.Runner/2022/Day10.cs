db5f46d4e1b20b617828b1d67ae6f3d4e269fc86
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-11-07 01:01:32 +0200

    2022/day-10

--- Puzzles.Runner/2022/Day10.cs
+++ Puzzles.Runner/2022/Day10.cs
@@ -1,10 +1,17 @@
-﻿namespace Puzzles.Runner._2022;
+﻿using System.Text;
 
-[Puzzle("Rope Bridge", 10, 2022)]
+namespace Puzzles.Runner._2022;
+
+[Puzzle("Cathode-Ray Tube", 10, 2022)]
 public class Day10(ILinesInputReader input) : IPuzzleSolver
 {
-    private record Instruction(string Opcode, int Value);
+    private enum OpCode
+    {
+        Noop,
+        Addx
+    };
 
+    private record Instruction(OpCode OpCode, int Value);
     private Instruction[] _program = [];
 
     public void Init()
@@ -12,50 +19,52 @@ public class Day10(ILinesInputReader input) : IPuzzleSolver
         _program = [..input.Lines.Select(line =>
         {
             var tokens = line.Split(' ', 2);
-            return new Instruction(tokens[0], tokens.Length > 1 ? Convert.ToInt32(tokens[1]) : 0);
+            return new Instruction
+            (
+                OpCode: Enum.Parse<OpCode>(tokens[0], true), 
+                Value: tokens.Length > 1 ? Convert.ToInt32(tokens[1]) : 0);
         })];
     }
 
     public string SolvePart1()
     {
-        var e = Run(_program).GetEnumerator();
+        var samples = new HashSet<int> { 20, 60, 100, 140, 180, 220 };
+        return Run(_program)
+            .WithIndex()
+            .Where(d => samples.Contains(d.index + 1))
+            .Sum(d => (d.index + 1) * d.item)
+            .ToString();
+    }
 
-        Loop(e, 20);
-        long sum = 20 * e.Current;
-        
-        for(int i = 1; i <= 5; i++)
+    public string SolvePart2()
+    {
+        var sb = new StringBuilder();
+
+        foreach (var (x, idx) in Run(_program).Select((d, i) => (d, i % 40)))
         {
-            Loop(e, 40);
-            sum += (20 + i * 40) * e.Current;
+            if(idx == 0)
+                sb.AppendLine();
+
+            sb.Append(Math.Abs(idx - x) <= 1 ? '#' : ' ');
         }
 
-        return sum.ToString();
+        return sb.ToString();
     }
 
-    private void Loop(IEnumerator<int> enumerator, int cycles)
-    {
-        for (int i = 0; i < cycles; i++)
-            enumerator.MoveNext();
-    }
-
-    private IEnumerable<int> Run(Instruction[] program) 
+    private static IEnumerable<int> Run(Instruction[] program) 
     {
         int x = 1;
-
         foreach (var instr in program)
         {
-            if(instr.Opcode == "noop")
+            if(instr.OpCode == OpCode.Noop)
                 yield return x;
 
-            if(instr.Opcode == "addx")
+            if(instr.OpCode == OpCode.Addx)
             {
                 yield return x;
                 yield return x;
                 x += instr.Value;
             }
         }
-
-        while (true)
-            yield return x;
     }
 }
