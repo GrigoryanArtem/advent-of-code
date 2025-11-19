a92288cf93609dabe81375f59a6b622b8d99d3f5
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-11-19 02:48:41 +0200

    2021/day-08/opt-1

--- Puzzles.Runner/2021/Day08.cs
+++ Puzzles.Runner/2021/Day08.cs
@@ -2,43 +2,47 @@
 
 [Puzzle("Seven Segment Search", 8, 2021)]
 public class Day08(ILinesInputReader input) : IPuzzleSolver
-{
-    private record Example(HashSet<char>[] Patterns, HashSet<char>[] Output);
+{    
+    private record Example(string[] Patterns, string[] Output);
 
     private class Resolver
     {
-        private HashSet<char>[] _numbers = new HashSet<char>[10];
-
-        public void Init(HashSet<char>[] patterns)
+        private readonly string[] _numbers = new string[10];
+        public int Resolve(string[] patterns, string[] probes)
         {
-            _numbers[1] = Find(patterns, 2);
-            _numbers[4] = Find(patterns, 4);
-            _numbers[7] = Find(patterns, 3);
-            _numbers[8] = Find(patterns, 7);
-
-            _numbers[2] = Find(patterns, _numbers, 5, 1, 2, 2);
-            _numbers[3] = Find(patterns, _numbers, 5, 2, 3, 3);
-            _numbers[5] = Find(patterns, _numbers, 5, 1, 3, 2);
-
-            _numbers[0] = Find(patterns, _numbers, 6, 2, 3, 3);
-            _numbers[6] = Find(patterns, _numbers, 6, 1, 3, 2);
-            _numbers[9] = Find(patterns, _numbers, 6, 2, 4, 3);
+            Init(patterns);
+            return probes.Aggregate(0, (acc, v) => acc * 10 + Resolve(v));
         }
 
-        public int Resolve(HashSet<char> probe)
-            => _numbers.WithIndex().Single(n => n.item.Count == probe.Count && !n.item.Except(probe).Any()).index;
+        private void Init(string[] patterns)
+        {
+            _numbers[1] = Find(patterns, 2);
+            _numbers[7] = Find(patterns, 3);
+            _numbers[4] = Find(patterns, 4);
+            _numbers[8] = Find(patterns, 7);
 
-        public int Resolve(HashSet<char>[] probes)
-            => probes.Aggregate(0, (acc, v) => acc * 10 + Resolve(v));
+            _numbers[2] = Find(patterns, _numbers, 5, 1, 2);
+            _numbers[3] = Find(patterns, _numbers, 5, 2, 3);
+            _numbers[5] = Find(patterns, _numbers, 5, 1, 3);
 
-        private static HashSet<char> Find(HashSet<char>[] patterns, int length)
-            => patterns.Single(p => p.Count == length);
+            _numbers[0] = Find(patterns, _numbers, 6, 2, 3);
+            _numbers[6] = Find(patterns, _numbers, 6, 1, 3);
+            _numbers[9] = Find(patterns, _numbers, 6, 2, 4);
+        }
 
-        private static HashSet<char> Find(HashSet<char>[] patterns, HashSet<char>[] precalc, int length, int one, int four, int seven)
-            => patterns.Single(p => p.Count == length &&
-                precalc[1].Intersect(p).Count() == one &&
-                precalc[4].Intersect(p).Count() == four &&
-                precalc[7].Intersect(p).Count() == seven);
+        private int Resolve(string probe)
+            => Array.IndexOf(_numbers, probe);
+
+        private static string Find(string[] patterns, int length)
+            => patterns.First(p => p.Length == length);
+
+        private static string Find(string[] patterns, string[] precalc, int length, int one, int four)
+            => patterns.First(p => p.Length == length &&
+                IntersectcCount(precalc[1], p) == one &&
+                IntersectcCount(precalc[4], p) == four);
+
+        private static int IntersectcCount(string a, string b)
+            => a.Count(b.Contains);
     }
 
     private Example[] _examples = [];
@@ -48,37 +52,29 @@ public class Day08(ILinesInputReader input) : IPuzzleSolver
         _examples = [.. input.Lines.Select(line =>
         {
             var tokens = line.Split("|");
-            var patterns = tokens[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var output = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
             return new Example
             (
-                [..patterns.Select(str => str.ToHashSet())],
-                [..output.Select(str => str.ToHashSet())]
+                [..tokens[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(str => new string([..str.OrderBy(x => x)]))],
+
+                [..tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(str => new string([..str.OrderBy(x => x)]))]
             );
         })];
     }
 
     public string SolvePart1()
-        => _examples.Sum(e => e.Output.Count(x => GetSimpleDigit(x) is not null)).ToString();
+        => _examples.Sum(e => e.Output.Count(IsSimpleDigit)).ToString();
 
     public string SolvePart2()
     {
         var resolver = new Resolver();
-        return _examples.Sum(ex =>
-        {
-            resolver.Init(ex.Patterns);
-            return resolver.Resolve(ex.Output);
-        }).ToString();
+        return _examples.Sum(ex => resolver.Resolve(ex.Patterns, ex.Output)).ToString();
     }
 
-    public static int? GetSimpleDigit(HashSet<char> value) => value.Count switch
+    public static bool IsSimpleDigit(string value) => value.Length switch
     {
-        2 => 1,
-        4 => 4,
-        3 => 7,
-        7 => 8,
-
-        _ => null
+        2 or 4 or 3 or 7 => true,
+        _ => false
     };
 }
