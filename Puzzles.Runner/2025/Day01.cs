24966ddea2ea1cf9453ab967768510ac1038dfd8
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2025-12-01 08:55:42 +0200

    2025/day-01

---
+++ Puzzles.Runner/2025/Day01.cs
@@ -0,0 +1,27 @@
+﻿namespace Puzzles.Runner._2025;
+
+[Puzzle("Secret Entrance", 1, 2025)]
+public partial class Day01(ILinesInputReader input) : IPuzzleSolver
+{
+    private const int MOD = 100;
+
+    public string SolvePart1()
+        => Sequance().Count(d => d.deal == 0).ToString();
+
+    public string SolvePart2()
+        => Sequance().Sum(d => d.zeros).ToString();
+
+    private IEnumerable<(int deal, int zeros)> Sequance()
+    {
+        var deal = 50;
+
+        foreach (var instr in input.Lines)
+        {
+            var nd = deal + (instr[0] == 'L' ? -1 : 1) * Int32.Parse(instr.AsSpan()[1..]);
+            int zeros = Math.Abs(nd) / MOD + (nd <= 0 && deal > 0 ? 1 : 0);
+
+            deal = AOC.Mod(nd, MOD);
+            yield return (deal, zeros);
+        }
+    }
+}
