a7c33ea0e3ec8ce7b1786e97a7c4388f115eb74b
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-04-09 02:36:19 +0300

    2018/day-04

---
+++ Puzzles.Runner/2018/Day04.cs
@@ -0,0 +1,123 @@
+﻿using System.Globalization;
+
+namespace Puzzles.Runner._2018;
+
+[Puzzle("Repose Record", 4, 2018)]
+public partial class Day04(ILinesInputReader input) : IPuzzleSolver
+{
+    private const string DT_FROMAT = "yyyy-MM-dd HH:mm";
+    private const int HOUR_MINS = 60;
+
+    private enum EventType
+    {
+        Shift,
+        FallsAsleep,
+        WakesUp
+    }
+
+    private struct Log
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public EventType Type { get; set; }
+    }
+
+    private Log[] _logs = [];
+
+    public void Init()
+        => _logs = [.. input.Lines.Select(Str2Log).OrderBy(l => l.DateTime)];    
+
+    public string SolvePart1()
+    {
+        var timeTable = CreateTimeTable(_logs);
+
+        var maxSum = timeTable.MaxBy(x => x.Value.Sum());
+        var idx = maxSum.Value.AsSpan().IndexOfMax();
+
+        return (maxSum.Key * idx).ToString();
+    }
+
+    public string SolvePart2()
+    {
+        var timeTable = CreateTimeTable(_logs);
+
+        var maxVal = 0;
+        var maxIdx = 0;
+        var guardId = 0;
+
+        foreach (var kv in timeTable)
+        {
+            foreach(var (sleep, idx) in kv.Value.WithIndex())
+            {
+                if(sleep > maxVal)
+                {
+                    maxVal = sleep;
+                    maxIdx = idx;
+                    guardId = kv.Key;
+                }
+            }
+        }             
+        
+
+        return (guardId * maxIdx).ToString();
+    }
+
+    private static Dictionary<int, int[]> CreateTimeTable(Log[] logs)
+    {
+        Dictionary<int, int[]> dict = [];
+
+        var id = 0;
+        var start = 0;
+
+        foreach (var log in logs)
+        {
+            if (log.Type == EventType.Shift)
+            {
+                dict.TryAdd(log.Id, new int[HOUR_MINS]);
+                id = log.Id;
+            }
+            else if (log.Type == EventType.FallsAsleep)
+            {
+                start = log.DateTime.Minute;
+            }
+            else
+            {                
+                for (int i = start; i < log.DateTime.Minute; i++)
+                    dict[id][i]++;
+            }
+        }
+
+        return dict;
+    }
+
+    private static Log Str2Log(string str)
+    {
+        var dtEnd= DT_FROMAT.Length + 1;
+        var dt = DateTime.ParseExact(str[1..dtEnd], DT_FROMAT, CultureInfo.InvariantCulture);
+
+        var eventStart = dtEnd + 2;
+        var eventStr = str[eventStart..];
+        var type = eventStr switch
+        {
+            "falls asleep" => EventType.FallsAsleep,
+            "wakes up" => EventType.WakesUp,
+            _ => EventType.Shift
+        };
+
+        int id = 0;
+        if(type == EventType.Shift)
+        {
+            var idStart = str.IndexOf('#');
+            id = 0;
+            for (int i = idStart + 1; str[i] != ' '; i++)
+                id = (id * 10) + (str[i] - '0');
+        }
+
+        return new() 
+        { 
+            DateTime = dt,
+            Id = id,
+            Type = type
+        };
+    }
+}
--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Time was measured by running each part n times and finding the average result. T
 ## Events
 | Year          |         ⭐ |
 | ------------- | --------: |
-| Total         |   **200** |
+| Total         |   **202** |
 | [2025](#2025) | **24**/24 |
 | [2024](#2024) | **50**/50 |
 | [2023](#2023) |  **8**/50 |
@@ -40,7 +40,7 @@ Time was measured by running each part n times and finding the average result. T
 | [2021](#2021) | **20**/50 |
 | [2020](#2020) | **18**/50 |
 | [2019](#2019) | **28**/50 |
-| [2018](#2018) |  **6**/50 |
+| [2018](#2018) |  **8**/50 |
 | [2017](#2017) |  **4**/50 |
 | [2016](#2016) |  **6**/50 |
 | [2015](#2015) | **10**/50 |
@@ -173,7 +173,8 @@ Time was measured by running each part n times and finding the average result. T
 | --- | ---: | ------------------------------------------------------------------ | ---------------: | ---------------: | ------------------------------------------------------------------------------------------------- |
 | 🟩   |   01 | [Chronal Calibration](https://adventofcode.com/2018/day/1)         |            0.001 |            1.541 | [Code](https://github.com/GrigoryanArtem/advent-of-code/blob/master/Puzzles.Runner/2018/Day01.cs) |
 | 🟩   |   02 | [Inventory Management System](https://adventofcode.com/2017/day/2) |            0.035 |           12.183 | [Code](https://github.com/GrigoryanArtem/advent-of-code/blob/master/Puzzles.Runner/2017/Day02.cs) |
-| 🟩   |   03 | [No Matter How You Slice It](https://adventofcode.com/2017/day/3) |                  |                  | [Code](https://github.com/GrigoryanArtem/advent-of-code/blob/master/Puzzles.Runner/2017/Day03.cs) |
+| 🟩   |   03 | [No Matter How You Slice It](https://adventofcode.com/2017/day/3)  |            2.441 |            1.495 | [Code](https://github.com/GrigoryanArtem/advent-of-code/blob/master/Puzzles.Runner/2017/Day03.cs) |
+| 🟩   |   04 | [Repose Record](https://adventofcode.com/2017/day/4)               |            0.028 |            0.020 | [Code](https://github.com/GrigoryanArtem/advent-of-code/blob/master/Puzzles.Runner/2017/Day04.cs) |
 
 
 ### 2017
